29c79766dd3050e9b73baf2f1b4b775728d79ec6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-24 16:54:02 +0530

    condition in job

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "network_vpc" {
-  source = "github.com/pcs1999/tf_module_vpc.git"
+  source     = "github.com/pcs1999/tf_module_vpc.git"
+  env        = var.env
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value.cidr_block
 }
