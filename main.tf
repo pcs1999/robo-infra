ee234586f975903e5c7fab20245c8473c22ccaad
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-24 16:21:46 +0530

    condition in job

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "null_resource" "just" {
+module "network_vpc" {
+  source = "github.com/pcs1999/tf_module_vpc.git"
+  version = "main"
 
-  provisioner "local-exec" {
-   command = "echo ${var.env}"
-  }
-
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
 }
