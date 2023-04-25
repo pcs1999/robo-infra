d79a8125f3bc6d028fb34361a09128e3758ac7b6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-04-25 12:49:14 +0530

    vpc_peering_tag

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "network_vpc" {
   source     = "github.com/pcs1999/tf_module_vpc.git"
   env        = var.env
+  default_vpc_id = var.default_vpc_id
 
   for_each   = var.vpc
   cidr_block = each.value.cidr_block
