75c778d8583980d72c544362b3b58920e34c0725
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-08 15:18:51 +0530

    subnet_groups

--- main.tf
+++ main.tf
@@ -20,7 +20,11 @@ module "docdb" {
   for_each = var.docdb
   subnet_ids = lookup(lookup(lookup(lookup(module.network_vpc, each.value.vpc_name, null), "private_subnets_ids", null), each.value.subnets_name, null), "subnet_id", null )
 // the subnet_ids is taking from output of module.network_vpc
+  allow_cidr = lookup(lookup(lookup(lookup(module.network_vpc, each.value.vpc_name, null), "private_subnets_ids", null), "app", null), "subnet_id", null )
+
+vpc_id = lookup(lookup(module.network_vpc, each.value.vpc_name , null), "vpc_id", null) // strings are in double quotes,expressions are not exp=each.value.vpc_name , strings="vpc_id"
 }
+
 
 
 output "network_vpc" {
