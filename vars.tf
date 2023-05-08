293b5a45cbe95987942d0aa7fc5a3c039fa55ced
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-08 13:11:18 +0530

    subnet_groups

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
-#variable "docdb" {}
+variable "docdb" {}
 #variable "public_subnets" {}
 #variable "internet_gw" {}
 #variable "nat_gw" {}
