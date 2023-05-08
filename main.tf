d4c57b1fbd1a7d5c0d97d1249f25abad6f497015
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-08 11:50:52 +0530

    subnet_groups

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "network_vpc" {
 module "docdb" {
   source = "github.com/pcs1999/tf-documentDB.git"
   env    = var.env
-  for_each = var.vpc
+  for_each = var.docdb
   subnet_ids = lookup(lookup(lookup(module.network_vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnets_ids", null )
 // the subnet_ids is taking from output of module.network_vpc
 }
