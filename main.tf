a7deb482614aedb50970ac80706ff6796e1ca63c
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-31 12:12:53 +0530

    iamges_creation

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ module "minikube" {
   cluster_name      = "minikube"
   aws_instance_type = "t3.medium"
   ssh_public_key    = "~/.ssh/id_rsa.pub"
-  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.network_vpc, "main", null), "public_subnet_ids" null), "public", null), "subnet_id", null ), 0)
+  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.network_vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_id", null ), 0)
   //ami_image_id        = data.aws_ami.ami.id
   hosted_zone         = var.Hosted_zone
   hosted_zone_private = false
