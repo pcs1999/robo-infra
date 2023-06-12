15b96aa4325017eaa268ff8dfad8b3452ae5dda5
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-06-12 11:12:25 +0530

    eks

--- main.tf
+++ main.tf
@@ -119,34 +119,46 @@ output "network_vpc" {
 }
 
 
-module "minikube" {
-  source = "github.com/scholzj/terraform-aws-minikube"
+#module "minikube" {
+#  source = "github.com/scholzj/terraform-aws-minikube"
+#
+#  aws_region        = "us-east-1"
+#  cluster_name      = "minikube"
+#  aws_instance_type = "t3.medium"
+#  ssh_public_key    = "~/.ssh/id_rsa.pub"
+#  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.network_vpc, "main", null), "public_subnets_ids", null), "public", null), "subnet_id", null ), 0)
+#  //ami_image_id        = data.aws_ami.ami.id
+#  hosted_zone         = var.Hosted_zone
+#  hosted_zone_private = false
+#
+#  tags = {
+#    Application = "Minikube"
+#  }
+#
+#  addons = [
+#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+#  ]
+#}
+#
+#output "MINIKUBE_SERVER" {
+#  value = "ssh centos@${module.minikube.public_ip}"
+#}
+#
+#output "KUBE_CONFIG" {
+#  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+#}
 
-  aws_region        = "us-east-1"
-  cluster_name      = "minikube"
-  aws_instance_type = "t3.medium"
-  ssh_public_key    = "~/.ssh/id_rsa.pub"
-  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.network_vpc, "main", null), "public_subnets_ids", null), "public", null), "subnet_id", null ), 0)
-  //ami_image_id        = data.aws_ami.ami.id
-  hosted_zone         = var.Hosted_zone
-  hosted_zone_private = false
 
-  tags = {
-    Application = "Minikube"
-  }
-
-  addons = [
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-  ]
-}
-
-output "MINIKUBE_SERVER" {
-  value = "ssh centos@${module.minikube.public_ip}"
-}
-
-output "KUBE_CONFIG" {
-  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+module "eks" {
+  source                 = "github.com/r-devops/tf-module-eks"
+  ENV                    = var.env
+  PRIVATE_SUBNET_IDS     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnets_ids", null), "app", null), "subnet_id", null)
+  PUBLIC_SUBNET_IDS      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnets_ids", null), "public", null), "subnet_id", null)
+  DESIRED_SIZE           = 2
+  MAX_SIZE               = 2
+  MIN_SIZE               = 2
+  CREATE_PARAMETER_STORE = true
 }
